02e28e36d7218a0fa8624a5b67864fde12ca3005
joelbarnum <joelbarnum@yahoo.com>
2021-02-05 13:28:22 -0800

    message-repo-dbcontext

---
+++ Models/Message.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Message
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DisplayName("Message ID")]
+        public Guid MessageID { get; set; }
+        [DisplayName("Sender ID")]
+        [ForeignKey("UserID")]
+        public Guid SenderID { get; set; }
+        [DisplayName("Recipient List ID")]
+        [ForeignKey("UserID")]
+        public Guid RecipientListID { get; set; }
+        [DisplayName("Sent Date")]
+        [DataType(DataType.DateTime)]
+        public DateTime SentDate { get; set; }
+        [DisplayName("Message Text")]
+        public string MessageText { get; set; }
+    }
+}
--- Repo/Repo.cs
+++ Repo/Repo.cs
@@ -1,8 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Models;
 
 namespace Repo
 {
     public class Repo
     {
+        private readonly ProgContext _progContext;
+        private readonly ILogger _logger;
+        public DbSet<Message> messages;
+
+        public Repo(ProgContext progContext, ILogger<Repo> logger)
+        {
+            _progContext = progContext;
+            _logger = logger;
+            this.messages = _progContext.Messages;
+
+        }
+
+        // Access SaveChanges from Logic class
+        public async Task CommitSave()
+        {
+            await _progContext.SaveChangesAsync();
+        }
+
+        public async Task<Message> GetMessageById(Guid id)
+        {
+            return await messages.FindAsync(id);
+        }
+        public async Task<IEnumerable<Message>> GetMessages()
+        {
+            return await messages.ToListAsync();
+        }
     }
 }
