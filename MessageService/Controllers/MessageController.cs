6e41cf044b3599c5e545b950457421aa62ebfb53
ranerijosh <43115830+ranerijosh@users.noreply.github.com>
2021-03-02 18:12:29 -0500

    Update MessageController.cs

--- MessageService/Controllers/MessageController.cs
+++ MessageService/Controllers/MessageController.cs
@@ -14,6 +14,7 @@ namespace MessageService.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class MessageController : ControllerBase
     {
         private readonly Logic _logic;
